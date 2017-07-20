5537952e4f3b3b4bf9082f8be2631f5483e540ca
toshi <toshi.mana.plus1@gmail.com>
2017-07-21 08:28:18 +0900

    what : refactoring

--- src/Lib.hs
+++ src/Lib.hs
@@ -76,8 +76,9 @@ createCandidates_ num constraint vol = Prelude.map Candidate (createCandidates__
 volume :: Constraint -> Int
 volume (Constraint cs) = sum (intersperse 1 cs)
 
-cvolume :: Constraint -> Int -> Int
-cvolume constraint@(Constraint cs) s =
+cvolume :: Constraint -> Range -> Int
+cvolume constraint@(Constraint cs) (Range lb ub) =
+    let s = ub - lb + 1 in
     let box = Prelude.length cs + 1 in
     let ball = s - volume constraint in
     f box ball
@@ -175,7 +176,7 @@ logicalLinesStep problem@(MProblem mb@(MBoard board) mrc mcc) line@(Line directi
     
                                
 logicalStep :: SolvingProblem -> IO (Maybe MProblem)
-logicalStep sp@(SolvingProblem problem (ChangeLines seql)) =
+logicalStep (SolvingProblem problem (ChangeLines seql)) =
     case viewl seql of 
       EmptyL -> return (Just problem)
       e :< es -> logicalLinesStep problem e >>= maybe (return Nothing) (nextLogicalStep (ChangeLines es))
@@ -191,8 +192,8 @@ logicalStep sp@(SolvingProblem problem (ChangeLines seql)) =
 choiceDirection :: Direction -> a -> a -> a
 choiceDirection (Direction d) a b = bool b a d
 
-getCells :: MBoard -> IO [Cell]
-getCells (MBoard mb) = getAssocs mb >>= return.(Prelude.map (\(a,b)->Cell a b))
+getCells :: IBoard -> [Cell]
+getCells (IBoard ib) = Prelude.map (\(a,b)->Cell a b) (assocs ib)
 
 rangeList :: Range -> [a] -> [a]
 rangeList (Range lb ub) xs = Prelude.drop (lb-1) $ Prelude.take ub xs
@@ -213,33 +214,48 @@ unconfirmedList :: [Cell] -> Candidate -> [Cell]
 unconfirmedList bl (Candidate candidate) = 
   L.foldl' (\cur (c,i) -> if isConfirmedCell c then cur else (renewCell c i):cur) [] (L.zip bl candidate)
 
+refineConstraint :: MConstraints -> Bool -> IO [(Direction, LineIndex, RangeConstraint)]
+refineConstraint (MConstraints mc) flg = do
+  as <- getAssocs mc 
+  return $ concatMap (\(i,Constraints cs)-> Prelude.map (\e -> (Direction flg,i,e)) cs) as
+
+getSmallestCostConstraint :: MConstraints -> MConstraints -> IO (Direction, LineIndex, RangeConstraint)
+getSmallestCostConstraint mrc mcc = do
+  rc <- refineConstraint mrc True
+  cc <- refineConstraint mcc False
+  return $ minimumBy (\a b -> compare (calcCost a) (calcCost b)) (rc++cc)
+  where
+    calcCost (_,_,(RangeConstraint cs r)) = cvolume cs r
+
+createRegionCell :: IBoard -> Direction -> LineIndex -> RangeConstraint -> [Cell]
+createRegionCell ib di li (RangeConstraint _ r) =
+  let cells = getCells ib in rangeList r $ createLineFromBoard cells di li
+
+createEstimateCells :: IBoard -> Direction -> LineIndex -> RangeConstraint -> [[Cell]]
+createEstimateCells ib di li constraint =
+  let targetCell = createRegionCell ib di li constraint in
+  let bl = BoardLine (Prelude.map cellElt targetCell) in
+  let newLines = Prelude.filter (adaptLine bl) (createCandidates constraint) in
+  Prelude.map (unconfirmedList targetCell) newLines
+
+rewriteConstraint :: MConstraints -> LineIndex -> RangeConstraint -> IO ()
+rewriteConstraint (MConstraints c) li constraint = do
+  Constraints cs <- readArray c li
+  writeArray c li (Constraints (L.delete constraint cs))
+
+cloneAndEstimateProblem :: IProblem -> Direction -> [Cell] -> IO SolvingProblem
+cloneAndEstimateProblem iproblem direction xs = do
+  mp@(MProblem mb _ _) <- thawProblem iproblem
+  newLines <- Prelude.mapM (writeCell mb direction) xs
+  return (SolvingProblem mp (ChangeLines (L.foldl' (\lines (bl,i,e)-> (Line bl i (CellIndices (T.singleton e))) <| lines) S.empty newLines)))
+
 estimateStep :: MProblem -> IO [SolvingProblem]
-estimateStep mproblem@(MProblem mb mrc@(MConstraints rc) mcc@(MConstraints cc)) = do
-  rassocs <- getAssocs rc
-  cassocs <- getAssocs cc
-  let cs = refineConstraint rassocs True ++ refineConstraint cassocs False
-  let (di,li,constraint@(RangeConstraint c r)) = minimumBy (\a b -> compare (calcCost a) (calcCost b)) cs
+estimateStep mproblem@(MProblem mb mrc mcc) = do
+  (di,li,constraint) <- getSmallestCostConstraint mrc mcc
   rewriteConstraint (choiceDirection di mrc mcc) li constraint
-  cells <- getCells mb
-  let targetCell = rangeList r $ createLineFromBoard cells di li
-  let bl = BoardLine (Prelude.map cellElt targetCell)
-  let newLines = Prelude.filter (adaptLine bl) (createCandidates constraint)
-  let newCells = Prelude.map (unconfirmedList targetCell) newLines
-  iproblem <- freezeProblem mproblem
-  Prelude.mapM ( g iproblem di ) newCells
-    where
-      refineConstraint as flg = concatMap (\(i,Constraints cs)-> Prelude.map (\e -> (Direction flg,i,e)) cs) as
---      f (_,_,c) = Prelude.length $ createCandidates c
-      calcCost (_,_,(RangeConstraint cs (Range lb ub))) = cvolume cs (ub-lb+1)
-      g iproblem direction xs = do
-        mp@(MProblem mb _ _) <- thawProblem iproblem
-        newLines <- Prelude.mapM (writeCell mb direction) xs
-        return (SolvingProblem mp (ChangeLines (L.foldl' (\lines (bl,i,e)-> (Line bl i (CellIndices (T.singleton e))) <| lines) S.empty newLines)))
-      rewriteConstraint :: MConstraints -> LineIndex -> RangeConstraint -> IO ()
-      rewriteConstraint (MConstraints c) li constraint = do
-          Constraints cs <- readArray c li
-          writeArray c li (Constraints (L.delete constraint cs))
-        
+  iproblem@(IProblem ib _ _) <- freezeProblem mproblem
+  Prelude.mapM ( cloneAndEstimateProblem iproblem di ) (createEstimateCells ib di li constraint)
+
 isSolved :: MBoard -> IO Bool
 isSolved (MBoard mb) = getElems mb >>= return . and . ( Prelude.map (\(CellElt n) -> isJust n) )
 
